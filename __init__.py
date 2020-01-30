3c0f8cd6ff6cbf38da5ca57ea757802774bdc7b5
Angelos Theodosiadis <angelos.theodosiadis@datarobot.com>
2020-01-30 13:23:36 +0100

    Fixing __init__ import statements

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
-from .core import WebLocust, Locust, TaskSet, task, mod_context
-from .exception import InterruptTaskSet, ResponseError, RescheduleTaskImmediately
-from .config import configure, locust_config, register_config
+from locust.core import WebLocust, Locust, TaskSet, task, mod_context
+from locust.exception import InterruptTaskSet, ResponseError, RescheduleTaskImmediately
+from locust.config import configure, locust_config, register_config
 
 __version__ = "0.8dr6"
